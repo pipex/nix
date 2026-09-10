8bf43b8704678ef291b6d889f998a403873b90f2
Felipe Lalanne <felipe@balena.io>
2026-09-10 12:22:03 -0300

    Remove oh-my-zsh theme

--- home/common/core.nix
+++ home/common/core.nix
@@ -8,8 +8,6 @@
     sha256 = "1aw9rmshjc2jd4wdziy19fr4bm6amk2syzcan2099pcs7nrmqmip";
   };
 
-  xdg.configFile."oh-my-zsh".source = ../../dotfiles/oh-my-zsh;
-
   # Clipboard bridge. The same script tmux uses as its copy-command, also exposed
   # as `clip` on PATH so that copying works outside tmux. Falls back to an OSC 52
   # escape sequence when no clipboard tool is reachable, which is what makes
--- home/common/shell.nix
+++ home/common/shell.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }: {
+{ pkgs, ... }: {
   programs.zsh = {
     enable = true;
     enableCompletion = true;
@@ -10,7 +10,6 @@
 
     oh-my-zsh = {
       enable = true;
-      custom = "${config.xdg.configHome}/oh-my-zsh";
       extraConfig = ''
         DISABLE_MAGIC_FUNCTIONS="true";
       '';
